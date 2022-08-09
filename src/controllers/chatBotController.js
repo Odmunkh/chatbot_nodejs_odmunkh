c6e4169cdf99929698dca1d72b8c05dade53d876
odmunkh <odmunkh.o@ddishtv.mn>
2022-08-09 19:07:06 +0800

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -2,23 +2,43 @@ require("dotenv").config();
 import request from "request";
 
 let postWebhook = (req, res) =>{
+
+    // Parse the request body from the POST
     let body = req.body;
+
+    // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
+
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+
+            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                 handleMessage(sender_psid, webhook_event.message);
+                handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
             }
+
         });
+
+        // Return a '200 OK' response to all events
         res.status(200).send('EVENT_RECEIVED');
+
     } else {
+        // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
+
 };
 
 let getWebhook = (req, res) => {
@@ -35,44 +55,15 @@ let getWebhook = (req, res) => {
         }
     }
 };
-
-function handleMessage(sender_psid, received_message) {
-    if (received_message.text) {
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" + received_message.text);
-    }
-    if (received_message.text === "hi") {
-        let msg = {"text": `hi too`}
-        callSendAPI(sender_psid, msg);
-    }
-}
-
-function handlePostback(sender_psid, received_postback) {
-    let payload = received_postback.payload;
-    switch (payload) {
-        case "GET_STARTED":
-            let msg = {"text": `payload bna get started`}
-            callSendAPI(sender_psid, msg);
-            break;
-        case "menu":
-            let msg1 = {"text": `menu deer darlaa`}
-            callSendAPI(sender_psid, msg1);
-            break;
-        case "call":
-            let msg2 = {"text": `lavlah deer darlaa`}
-            callSendAPI(sender_psid, msg2);
-            break;
-    }
-}
-
-// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
         "recipient": {
             "id": sender_psid
         },
-        message: response
-    };
+        "message": response
+    }
+
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -81,11 +72,70 @@ function callSendAPI(sender_psid, response) {
         "json": request_body
     }, (err, res, body) => {
         if (!err) {
-            console.log('message sent!');
+            console.log('message sent!')
         } else {
             console.error("Unable to send message:" + err);
         }
     });
+}
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Send the response message
+    callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 module.exports = {
