75f55299d9b46fd0434c1d7f06f54d6cc29852ca
odmunkh <odmunkh.o@ddishtv.mn>
2022-08-09 19:35:45 +0800

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -2,43 +2,23 @@ require("dotenv").config();
 import request from "request";
 
 let postWebhook = (req, res) =>{
-
-    // Parse the request body from the POST
     let body = req.body;
-
-    // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
-
-            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-
-
-            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
-
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
+                 handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
             }
-
         });
-
-        // Return a '200 OK' response to all events
         res.status(200).send('EVENT_RECEIVED');
-
     } else {
-        // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
-
 };
 
 let getWebhook = (req, res) => {
@@ -56,96 +36,46 @@ let getWebhook = (req, res) => {
     }
 };
 
-function sendMessage (sender_psid, response) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let request_body = {
-                "recipient": {
-                    "id": sender_psid
-                },
-                "message": response,
-            };
-            // Send the HTTP request to the Messenger Platform
-            request({
-                "uri": "https://graph.facebook.com/v6.0/me/messages",
-                "qs": {"access_token": process.env.FB_PAGE_TOKEN},
-                "method": "POST",
-                "json": request_body
-            }, (err, res, body) => {
-                console.log(res)
-                console.log(body)
-                if (!err) {
-                    console.log("message sent!");
-                    resolve('done!')
-                } else {
-                    console.log("MESSAGE ILGEEHED ALDAA GARLAA" + err);
-                }
-            });
-        } catch (e) {
-            reject(e);
+function handleMessage(sender_psid, received_message) {
+    if (received_message.text === "hi") {
+        callSendAPI(sender_psid, `hi too`);
+    }
+}
+
+function handlePostback(sender_psid, received_postback) {
+    let payload = received_postback.payload;
+    switch (payload) {
+        case "GET_STARTED":
+            callSendAPI(sender_psid, `payload bna get started`);
+            break;
+        case "menu":
+            callSendAPI(sender_psid, `menu deer darlaa`);
+            break;
+        case "call":
+            callSendAPI(sender_psid, `lavlah deer darlaa`);
+            break;
+    }
+}
+
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error("Unable to send message:" + err);
         }
     });
-}
-
-
-async function handleMessage(sender_psid, received_message) {
-    let response;
-    console.log('aaaaaaaaaaaa' + received_message.text);
-    // Checks if the message contains text
-    if (received_message.text) {
-        // Create the payload for a basic text message, which
-        // will be added to the body of our request to the Send API
-        response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-        }
-    } else if (received_message.attachments) {
-        // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
-    }
-
-    // Send the response message
-    await sendMessage(sender_psid, response);
-}
-
-async function handlePostback(sender_psid, received_postback) {
-    let response;
-
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = {"text": "Thanks!"}
-    } else if (payload === 'no') {
-        response = {"text": "Oops, try sending another image."}
-    }
-    // Send the message to acknowledge the postback
-    await sendMessage(sender_psid, response);
 }
 
 module.exports = {
